15aa2d9080431c3d519136322807c1b25421cecb
taru-s <taru.saarelainen@helsinki.fi>
2022-04-05 18:11:53 +0300

    added fabric_service_test.py, added test_added_fabric_returned

--- fabric-stash/src/tests/fabric_service_test.py
+++ fabric-stash/src/tests/fabric_service_test.py
@@ -11,5 +11,5 @@ class TestFabricService(unittest.TestCase):
     def test_added_fabric_returned(self):
         self.service.add_fabric("name", 100, 100, False)
         returned = self.service.get_fabrics()
-        self.assertEqual("name, 100cm x 100cm, washed: no", "no")
+        self.assertEqual("name, 100cm x 100cm, washed: no", returned[0])
         

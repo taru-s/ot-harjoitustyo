6cbe893207a94d37eacbdea3365e851c404c280b
taru-s <taru.saarelainen@helsinki.fi>
2022-05-03 15:06:32 +0300

    gui.py formatting and doc-strings

--- fabric-stash/src/ui/gui.py
+++ fabric-stash/src/ui/gui.py
@@ -12,6 +12,8 @@ from entities.fabric import Fabric
 
 
 class GUI:
+    """Class for managing the Graphical User Interface
+    """
     def __init__(self, root):
         self._root = root
         self._current_view = None
@@ -24,57 +26,22 @@ class GUI:
         if self._current_view:
             self._current_view.destroy()
 
+
+    # general handlers
+
     def _add_fabric(self):
+        """Adds a 'dummy fabric' to the database, and shows it in edit view.
+        """
         self._service.add_fabric("",0,0,0)
         all_ids = self._service.get_all_ids()
         self._show_fabric_edit_view(all_ids[-1], True)
+    
 
-    def _handle_list_view_add_fabric(self):
-        self._add_fabric()
-
-    def _handle_list_view_search(self):
-        print("search")
-        self._show_fabric_search_view()
-
-    def _handle_list_view_logout(self):
-        print("logged out")
-
-    def _handle_list_view_show_fabric(self, *fabric_id):
-        self._show_fabric_info_view(fabric_id[0])
-
-    def _handle_info_view_edit_fabric(self, *fabric_id):
-        self._show_fabric_edit_view(fabric_id[0], False)
-
-    def _handle_info_view_delete_fabric(self, *fabric_id):
-        self._handle_delete_fabric(fabric_id[0])
-
-    def _handle_info_view_back(self):
-        self._show_fabric_list_view()
-
-    def _handle_edit_view_save(self, fabric_id, name, width, length, washed_bool):
-        washed = 0
-        if washed_bool:
-            washed = 1
-        self._service.edit_fabric(fabric_id, 
-                                    name, 
-                                    width, 
-                                    length, 
-                                    washed
-                                    )
-
-        self._show_fabric_info_view(fabric_id)
-
-    def _handle_edit_view_delete(self, *fabric_id):
-        self._handle_delete_fabric(fabric_id[0])
-
-    def _handle_edit_view_cancel(self, from_add_new, *fabric_id):
-        if from_add_new:
-            self._service.delete_fabric_by_id(fabric_id[0])
-            self._show_fabric_list_view()
-        else:
-            self._show_fabric_info_view(fabric_id[0])
-
-    def _handle_delete_fabric(self, *fabric_id):
+    def _delete_fabric(self, *fabric_id):
+        """Asks for confirmation for deleting the given fabric.
+        If user confirms, deletes the fabric and shows the list view.
+        If user declines, doesn't do anything.
+        """
         fabric_name = self._service.get_fabric_by_id(fabric_id[0]).name
         delete_confirmation = messagebox.askyesno(
             title="delete fabric",
@@ -85,16 +52,73 @@ class GUI:
             self._service.delete_fabric_by_id(fabric_id[0])
             self._show_fabric_list_view()
 
+
+    # list view handlers
+
+    def _handle_list_view_add_fabric(self):
+        self._add_fabric()
+
+    def _handle_list_view_search(self):
+        self._show_fabric_search_view()
+
+    def _handle_list_view_logout(self):
+        print("logged out")
+
+    def _handle_list_view_show_fabric(self, *fabric_id):
+        self._show_fabric_info_view(fabric_id[0])
+
+
+    # info view handlers
+
+    def _handle_info_view_edit_fabric(self, *fabric_id):
+        self._show_fabric_edit_view(fabric_id[0], False)
+
+    def _handle_info_view_delete_fabric(self, *fabric_id):
+        self._delete_fabric(fabric_id[0])
+
+    def _handle_info_view_back(self):
+        self._show_fabric_list_view()
+
+    
+    # edit view handlers
+
+    def _handle_edit_view_save(self, fabric_id, name, width, length, washed_bool):
+        washed = 0
+        if washed_bool:
+            washed = 1
+        self._service.edit_fabric(fabric_id,
+                                  name,
+                                  width,
+                                  length,
+                                  washed
+                                  )
+
+        self._show_fabric_info_view(fabric_id)
+
+    def _handle_edit_view_delete(self, *fabric_id):
+        self._delete_fabric(fabric_id[0])
+
+    def _handle_edit_view_cancel(self, from_add_new, *fabric_id):
+        if from_add_new:
+            self._service.delete_fabric_by_id(fabric_id[0])
+            self._show_fabric_list_view()
+        else:
+            self._show_fabric_info_view(fabric_id[0])
+
+    
+    # search view handlers
+
     def _handle_search_view_add(self):
         self._add_fabric()
-        print("add") 
 
     def _handle_search_view_list(self):
         self._show_fabric_list_view()
-        print("list")
 
     def _handle_search_view_show_fabric(self, *fabric_id):
         self._show_fabric_info_view(fabric_id[0])
+
+
+    # 'show view' methods
 
     def _show_fabric_list_view(self):
         self._hide_current_view()
@@ -122,7 +146,16 @@ class GUI:
 
         self._current_view.pack()
 
-    def _show_fabric_edit_view(self, fabric_id, from_add_new):
+    def _show_fabric_edit_view(self, fabric_id: int, from_add_new: bool):
+        """Shows fabric in edit view.
+
+        Args:
+            fabric_id (int): id of the fabric to be shown
+            from_add_new (bool): A boolean value for whether the user is coming to the edit view
+                    from adding a new fabric, or not. Used for deciding what to do when cancelling
+                    out of the edit view.
+        """
+
         self._hide_current_view()
 
         self._current_view = FabricEditView(

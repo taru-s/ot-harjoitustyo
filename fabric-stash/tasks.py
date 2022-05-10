4e739ead9c316d09a8f95ede811111d2d9079c8b
taru-s <taru.saarelainen@helsinki.fi>
2022-05-10 13:15:35 +0300

    poetry invoke fo initializing the database

--- fabric-stash/tasks.py
+++ fabric-stash/tasks.py
@@ -23,3 +23,8 @@ def format(ctx):
 @task
 def lint(ctx):
     ctx.run("pylint src", pty=True)
+
+@task
+def initialize_database(ctx):
+    ctx.run("python3 src/initialize_database.py", pty=True)
+    
